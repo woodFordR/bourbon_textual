fd0933cc8b283ceb11c8976a52cf04f6f6f83aad
woodFordR <adam@kaewell.com>
2025-04-06 16:04:30 -0400

    adding reactivity, added details to README, updated Inputs for changing reactive Model

---
+++ README.md
@@ -0,0 +1,18 @@
+## The Bourbon 🥃 Textual App
+---
+## Woodford <woody@woodford.life>
+---
+### Application Development
+
+ - creation of a python textual application
+ - sqlmodel with typing for read, update models
+ - simple input and tree widgets with horizontal containers
+
+---
+### How to run
+
+ - `poetry shell`
+ - `poetry install`
+ - `poetry run python bourbon/main.py`
+
+---
--- bourbon/main.py
+++ bourbon/main.py
@@ -2,6 +2,7 @@ import uuid
 
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, VerticalScroll
+from textual.reactive import reactive
 from textual.widgets import Footer, Header, Input, Markdown
 
 from bourbon.models.types import MacOS
@@ -21,24 +22,34 @@ new_mac = MacOS(
 
 class BourbonApp(App):
 
+    mac_os: reactive[MacOS] = reactive(MacOS)
+
+    def __init__(self, new_mac: MacOS):
+        self.mac_os = MacOS.model_validate(new_mac)
+
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True, icon="🥃")
-        yield Footer()
-        yield Input(placeholder="enter a word", id="word-enter")
+        yield Input(placeholder="enter name", name="name")
+        yield Input(placeholder="enter IP", name="public_ip")
+        yield Input(placeholder="enter status", name="status")
+        yield Input(placeholder="enter memory", name="memory")
+        yield Input(placeholder="enter operating system", name="mac_os")
         with VerticalScroll(id="response-container"):
             yield Markdown(id="response")
         with Horizontal(id="data-tree"):
-            yield Mac(new_mac)
+            yield Mac(new_mac).data_bind(BourbonApp.mac_os)
+        yield Footer()
 
-    async def on_input_changed(self, message: Input.Changed) -> None:
-        if message.value:
-            await self.query_one("#response", Markdown).update(
-                f"{message.value} sucks."
-            )
+    async def on_input_submitted(self, event: Input.Submitted, input: Input) -> None:
+        if input.Submitted.value:
+            await self.update_mac_os(str(input.name) or "", input.Submitted.value)
         else:
             await self.query_one("#response", Markdown).update(f"you suck.")
 
+    async def update_mac_os(self, name: str, value: str):
+        setattr(self.mac_os, name, value)
+
 
 if __name__ == "__main__":
-    app = BourbonApp()
+    app = BourbonApp(new_mac)
     app.run()
